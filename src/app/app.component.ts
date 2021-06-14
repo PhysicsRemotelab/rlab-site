d23d6bf7ce34eb2fdb2123ae50e4a0653dee11c1
Toomas Tahves <toomastahves@hotmail.com>
2021-06-14 21:06:22 +0300

    refactor: remove app service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 import { Subscription } from 'rxjs';
-import { AppServices } from './app.services';
 import { User } from './modules/users/model';
 import { roles } from '../../src/environments/environment';
+import { UsersService } from './modules/users/state/users.services';
 
 @Component({
   selector: 'app-root',
@@ -17,7 +17,7 @@ export class AppComponent implements OnInit, OnDestroy {
 
   constructor(
     private auth: AuthService,
-    private appServices: AppServices
+    private usersService: UsersService
   ) {
   }
 
@@ -39,7 +39,7 @@ export class AppComponent implements OnInit, OnDestroy {
           picture: result.picture
         } as User;
   
-        this.appServices.postUser(user).subscribe((res: User) => {
+        this.usersService.postUser(user).subscribe((res: User) => {
           sessionStorage.setItem('user_id', res.id);
         });
       }
