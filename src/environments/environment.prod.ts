184ff0a3c142ec7179e4116dceb41f3c61793b99
Toomas Tahves <toomastahves@hotmail.com>
2021-06-15 19:49:47 +0300

    refactor: remove sensitive information

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const production = true;
-export const domain = "remotelab.eu.auth0.com";
-export const clientId = "5MROr4sw5rIlD73DXcb6051wQZqi4M53";
-export const audience = "https://remotelab.ee/";
-export const roles = "https://remotelab.ee/roles";
-export const serverUrl = "https://remotelab.ee";
-export const sensorUrl = "wss://remotelab.ee";
--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const production = false;
-export const domain = "remotelab.eu.auth0.com";
-export const clientId = "5MROr4sw5rIlD73DXcb6051wQZqi4M53";
-export const audience = "https://remotelab.ee/";
-export const roles = "https://remotelab.ee/roles";
-export const serverUrl = "http://localhost:80";
-export const sensorUrl = "ws://localhost:80";
