7cbd0943e15c8bce80f2dc2e9f4204a54b56d1c6
Toomas Tahves <toomastahves@hotmail.com>
2022-04-04 18:49:03 +0300

    fix: temperature plot graph

--- src/app/components/temperature-plot/temperature-plot.component.ts
+++ src/app/components/temperature-plot/temperature-plot.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, EventEmitter, OnChanges, OnDestroy, Output, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
 import { Chart } from 'chart.js';
 import { interval, Subscription } from 'rxjs';
 import { Input } from '@angular/core';
@@ -9,7 +9,7 @@ import { cameraUrl } from 'src/environments/environment';
     templateUrl: './temperature-plot.component.html',
     styleUrls: ['./temperature-plot.component.scss']
 })
-export class TemperaturePlotComponent implements OnDestroy, AfterViewInit, OnChanges {
+export class TemperaturePlotComponent implements OnDestroy, OnInit, AfterViewInit {
     @ViewChild('chart')
     private chartRef: ElementRef;
 
@@ -27,11 +27,9 @@ export class TemperaturePlotComponent implements OnDestroy, AfterViewInit, OnCha
 
     private chart: Chart;
     private points = [];
-    private dataSourceSubscription: Subscription = new Subscription();
+    private dataSourceSubscription: Subscription;
 
-    constructor() {}
-
-    ngAfterViewInit(): void {
+    ngAfterViewInit() {
         this.chart = new Chart(this.chartRef.nativeElement, {
             type: 'scatter',
             data: {
@@ -40,6 +38,10 @@ export class TemperaturePlotComponent implements OnDestroy, AfterViewInit, OnCha
                         data: this.points,
                         fill: false,
                         pointRadius: 1,
+                        pointBackgroundColor: 'black',
+                        pointBorderColor: 'black',
+                        pointHoverBackgroundColor: 'black',
+                        pointHoverBorderColor: 'black',
                         label: 'Pixel values'
                     }
                 ]
@@ -53,50 +55,47 @@ export class TemperaturePlotComponent implements OnDestroy, AfterViewInit, OnCha
                     },
                     y: {
                         min: 0,
-                        max: 255,
+                        max: 255
                     }
                 }
             }
         });
     }
 
-    ngOnChanges() {
-        console.log('ngOnChanges');
-        const source = interval(5000);
-
-        if (!this.measurementStarted) {
-            this.dataSourceSubscription.unsubscribe();
-            return;
+    ngOnInit() {
+        // prevent multiple subscriptions
+        if (!this.dataSourceSubscription) {
+            this.dataSourceSubscription = interval(5000).subscribe(() => this.updateData());
         }
+    }
 
-        this.dataSourceSubscription = source.subscribe((point: number) => {
-            console.log(point);
-            let self = this;
-            var img = new Image();
-            img.crossOrigin = 'Anonymous';
-            img.src = `${cameraUrl}/camera/0`;
-            const canvas = <HTMLCanvasElement>document.createElement('canvas');
-            canvas.width = 320;
-            canvas.height = 240;
-            const context = canvas.getContext('2d');
-            context.drawImage(img, 0, 0);
+    private async updateData() {
+        let self = this;
+        var img = new Image();
+        img.crossOrigin = 'Anonymous';
+        img.src = `${cameraUrl}/camera/0`;
+        const canvas = <HTMLCanvasElement>document.createElement('canvas');
+        canvas.width = 320;
+        canvas.height = 240;
+        const context = canvas.getContext('2d');
+        context.drawImage(img, 0, 0);
 
-            img.onload = function () {
-                console.log('onload');
-                const points = [];
-                for (let i = 0; i < 320; i++) {
-                    const point = context.getImageData(i, 120, 1, 1).data[0];
-                    const nr = { x: i, y: Number(point) };
-                    points.push(nr);
-                }
-                self.points = points;
-                self.chart.data.datasets[0].data = null;
-                self.chart.data.datasets[0].data = points;
-                self.chart.clear();
-                self.chart.update();
-                console.log(self.points);
-            };
-        });
+        img.onload = function () {
+            const points = [];
+            for (let i = 0; i < 320; i++) {
+                const point = context.getImageData(i, 120, 1, 1).data[0];
+                const nr = { x: i, y: Number(point) };
+                points.push(nr);
+            }
+            self.points = points;
+            // prevent multiple subscriptions
+            img.onload = function () {};
+        };
+        self.chart.data.datasets[0].data = null;
+        self.chart.data.datasets[0].data = this.points;
+        self.chart.clear();
+        self.chart.update();
+        console.log(self.points);
     }
 
     ngOnDestroy(): void {
