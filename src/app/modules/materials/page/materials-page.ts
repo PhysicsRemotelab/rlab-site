f4e9f875ae7c1135829fe51921a1f597fcb8ddf6
Toomas Tahves <toomastahves@hotmail.com>
2022-05-15 16:18:56 +0300

    feat: update materials, add docx

--- src/app/modules/materials/page/materials-page.ts
+++ src/app/modules/materials/page/materials-page.ts
@@ -9,33 +9,39 @@ export class MaterialsPageComponent implements OnInit {
     files = [
         {
             id: 1,
-            fileName: 'Efficiency of light-emitting diode.pdf',
-            location: '/assets/Efficiency of light-emitting diode.pdf'
+            fileName: 'Efficiency of light-emitting diode',
+            pdfLocation: '/assets/Efficiency of light-emitting diode.pdf',
+            docLocation: '/assets/Efficiency of light-emitting diode.docx'
         },
         {
             id: 2,
-            fileName: 'Light spectrum of lamp.pdf',
-            location: '/assets/Light spectrum of lamp.pdf'
+            fileName: 'Light spectrum of lamp',
+            pdfLocation: '/assets/Light spectrum of lamp.pdf',
+            docLocation: '/assets/Light spectrum of lamp.docx'
         },
         {
             id: 3,
-            fileName: 'Gamma spectroscopy.pdf',
-            location: '/assets/Gamma spectroscopy.pdf'
+            fileName: 'Gamma spectroscopy',
+            pdfLocation: '/assets/Gamma spectroscopy.pdf',
+            docLocation: '/assets/Gamma spectroscopy.docx'
         },
         {
             id: 4,
-            fileName: 'X-Ray Fluorescence spectroscopy.pdf',
-            location: '/assets/X-Ray Fluorescence spectroscopy.pdf'
+            fileName: 'X-Ray Fluorescence spectroscopy',
+            pdfLocation: '/assets/X-Ray Fluorescence spectroscopy.pdf',
+            docLocation: '/assets/X-Ray Fluorescence spectroscopy.docx'
         },
         {
             id: 5,
-            fileName: 'Effect of temperature on resistance.pdf',
-            location: '/assets/Effect of temperature on resistance.pdf'
+            fileName: 'Effect of temperature on resistance',
+            pdfLocation: '/assets/Effect of temperature on resistance.pdf',
+            docLocation: '/assets/Effect of temperature on resistance.docx'
         },
         {
             id: 6,
-            fileName: 'Diffraction of light.pdf',
-            location: '/assets/Diffraction of light.pdf'
+            fileName: 'Diffraction of light',
+            pdfLocation: '/assets/Diffraction of light.pdf',
+            docLocation: '/assets/Diffraction of light.docx'
         }
     ];
 
