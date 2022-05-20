f7f29f126c0f9be70941d2bf6a89f0631d4f5793
Toomas Tahves <toomastahves@hotmail.com>
2022-05-20 18:51:39 +0300

    feat: calculate grayscale of colored image

--- src/app/components/temperature-plot/temperature-plot.component.ts
+++ src/app/components/temperature-plot/temperature-plot.component.ts
@@ -85,8 +85,11 @@ export class TemperaturePlotComponent implements OnDestroy, OnInit, AfterViewIni
         img.onload = function () {
             const points = [];
             for (let i = 0; i < 320; i++) {
-                const point = context.getImageData(i, 120, 1, 1).data[0];
-                const nr = { x: i, y: Number(point) };
+                const red = context.getImageData(i, 120, 1, 1).data[0];
+                const green = context.getImageData(i, 120, 1, 1).data[1];
+                const blue = context.getImageData(i, 120, 1, 1).data[2]
+                const gray = 0.299 * red + 0.587 * green + 0.114 * blue;
+                const nr = { x: i, y: Number(gray) };
                 points.push(nr);
             }
             self.points = points;
